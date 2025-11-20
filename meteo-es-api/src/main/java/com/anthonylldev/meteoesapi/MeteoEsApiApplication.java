aaf42d4f8f099f4a195c4bb589af4fd5a5908244
Anthony Leon <anthonyfabian22lldev@gmail.com>
2025-11-20 01:31:47 +0100

    Initial commit

---
+++ meteo-es-api/src/main/java/com/anthonylldev/meteoesapi/MeteoEsApiApplication.java
@@ -0,0 +1,13 @@
+package com.anthonylldev.meteoesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoEsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoEsApiApplication.class, args);
+	}
+
+}
---
+++ meteo-es-api/src/test/java/com/anthonylldev/meteoesapi/MeteoEsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonylldev.meteoesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoEsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
