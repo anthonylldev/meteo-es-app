90010d34f3521cc7eec0343e1de1c92e2b0722e1
Anthony Leon <anthonyfabian22lldev@gmail.com>
2025-11-21 20:23:17 +0100

    feat(backend): configure CORS

--- meteo-es-api/src/main/java/com/anthonylldev/meteoesapi/MeteoEsApiApplication.java
+++ meteo-es-api/src/main/java/com/anthonylldev/meteoesapi/MeteoEsApiApplication.java
@@ -1,16 +1,17 @@
 package com.anthonylldev.meteoesapi;
 
 import com.anthonylldev.meteoesapi.infrastructure.config.properties.AemetProperties;
+import com.anthonylldev.meteoesapi.infrastructure.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(AemetProperties.class)
+@EnableConfigurationProperties({AemetProperties.class, CorsProperties.class})
 @SpringBootApplication
 public class MeteoEsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeteoEsApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MeteoEsApiApplication.class, args);
+    }
 
 }
